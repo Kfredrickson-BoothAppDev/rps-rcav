c68030c63f564ae7e24a1245fa789b5132c1fa0f
Kyle Fredrickson <kyle.fredrickson@chicagobooth.edu>
2023-04-11 11:33:38 +0000

    Rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,19 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+    def play_rock
+      @comp_move = ["rock", "paper", "scissors"].sample
+      
+      if @comp_move == "rock"
+        @outcome = "tied"
+      elsif @comp_move == "paper"
+        @outcome =  "lost"
+      else
+        @outcome = "won"
+      end
+      
+      render({ :template => "game_templates/user_rock.html.erb" })
+    end 
+
 
 end
